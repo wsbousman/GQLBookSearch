6ddb4d655587c9ab9e52e91cec35afb6dd3d6a12
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-12-06 01:25:32 -0600

    Up1
    Server.js, typeDefs, resolvers, schema index

--- server/server.js
+++ server/server.js
@@ -2,14 +2,33 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+// implement the Apollo Server and apply it to the Express server as middleware
+const startServer = async () => {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
